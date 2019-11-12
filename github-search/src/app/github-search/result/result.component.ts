b4b6e89c6b03659d26c18eb131a633268dc0d9cd
unknown <prathamk25@gmail.com>
2019-11-12 11:17:17 +0530

    Corrected UI

--- github-search/src/app/github-search/result/result.component.ts
+++ github-search/src/app/github-search/result/result.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input, ViewEncapsulation } from '@angular/core';
 import { IUserResult } from '../iuser-result';
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
-  styleUrls: ['./result.component.scss']
+  styleUrls: ['./result.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ResultComponent implements OnInit {
   @Input() searchResult: IUserResult;
 
   pageSize = 3;
   page = 1;
+  showDetails = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onShowDetails(item: IUserResult){
+    this.showDetails = !this.showDetails;
   }
 
 }
